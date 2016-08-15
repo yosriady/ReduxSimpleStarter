318a81997f087b6ee896ea77f3bc1ec6f9255f8b
Yos Riady <yosriady@gmail.com>
2016-08-15 09:17:35 +0800

    Chapter 4, Section 41

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript the Good Parts'},
+    {title: 'Society of Mind'},
+    {title: 'Brown Dogs & Barbers'},
+    {title: 'Ready Player One'},
+  ];
+}
